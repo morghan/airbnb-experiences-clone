8808e71bec1e336792a1b44b76b0fbb380616ae7
morghan <joel.hz24@gmail.com>
2022-03-11 15:36:18 -0600

    Conditional rendering - cardBadge

--- src/App.js
+++ src/App.js
@@ -2,20 +2,28 @@ import React from 'react';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
 import Card from './components/card/Card';
+import cardData from './data';
 
 export default function App() {
+	const Cards = cardData.map((card) => {
+		return (
+			<Card
+				key={card.id}
+				coverImg={card.coverImg}
+				rating={card.stats.rating}
+				reviewCount={card.stats.reviewCount}
+				location={card.location}
+				title={card.title}
+				price={card.price}
+				openSpots={card.openSpots}
+			/>
+		);
+	});
 	return (
 		<div>
 			<Navbar />
 			<Hero />
-			<Card
-				img='katie-zaferes.png'
-				rating='5.0'
-				reviewCount={6}
-				country='USA'
-				title='Life lessons with Katie Zaferes'
-				price={136}
-			/>
+			<section className='cardsSection'>{Cards}</section>
 		</div>
 	);
 }
--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,20 +2,31 @@ import React from 'react';
 import './Card.css';
 
 export default function Card(props) {
-	const photo = require('./' + props.img);
+	//It's cleaner to use template literals over concatenation
+	const photo = require(`../../images/${props.coverImg}`);
 	const star = require('./star.png');
+
+	//Logic for displaying the correct badge text
+	let badgeText;
+	if (props.openSpots === 0) {
+		badgeText = 'SOLD OUT';
+	} else if (props.location === 'Online') {
+		badgeText = 'ONLINE';
+	}
+
 	return (
 		<div className='card'>
+			{badgeText && <div className='cardBadge'>{badgeText}</div>}
 			<img src={photo} className='cardImage' alt='nop' />
 			<div className='cardInfo'>
 				<div className='cardRating'>
 					<img src={star} alt='' />
 					<span>{props.rating}</span>
 					<span className='cardReviews'>
-						({props.reviewCount}) • {props.country}
+						({props.reviewCount}) • {props.location}
 					</span>
 				</div>
-				<p>{props.title}</p>
+				<p className='cardTitle'>{props.title}</p>
 				<p>
 					<span className='cardCost'>From ${props.price}</span> / person
 				</p>
